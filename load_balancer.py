38f8baa5f2c6f9028988c4548389edbe2debd67c
TimB44 <tblamires22@gmail.com>
2025-03-25 16:19:16 -0600

    Fix error add log lines

--- load_balancer.py
+++ load_balancer.py
@@ -1,23 +1,26 @@
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
 from pox.lib.addresses import IPAddr, EthAddr
+
 log = core.getLogger()
 
 last_node_used = 5
 virtual_ip = IPAddr("10.0.0.10")
-next_server = ip_5_server
 ip_5_server = IPAddr("10.0.0.5")
 ip_6_server = IPAddr("10.0.0.6")
+next_server = ip_5_server
 ip_to_mac = {
-        IPAddr("10.0.0.1"): EthAddr("00:00:00:00:00:01"),
-        IPAddr("10.0.0.2"): EthAddr("00:00:00:00:00:02"),
-        IPAddr("10.0.0.3"): EthAddr("00:00:00:00:00:03"),
-        IPAddr("10.0.0.4"): EthAddr("00:00:00:00:00:04"),
-        IPAddr("10.0.0.5"): EthAddr("00:00:00:00:00:05"),
-        IPAddr("10.0.0.6"): EthAddr("00:00:00:00:00:06"),
-        }
+    IPAddr("10.0.0.1"): EthAddr("00:00:00:00:00:01"),
+    IPAddr("10.0.0.2"): EthAddr("00:00:00:00:00:02"),
+    IPAddr("10.0.0.3"): EthAddr("00:00:00:00:00:03"),
+    IPAddr("10.0.0.4"): EthAddr("00:00:00:00:00:04"),
+    IPAddr("10.0.0.5"): EthAddr("00:00:00:00:00:05"),
+    IPAddr("10.0.0.6"): EthAddr("00:00:00:00:00:06"),
+}
+
 
 def swap_server():
+    global next_server
     if next_server == ip_5_server:
         next_server = ip_6_server
     else:
@@ -25,19 +28,23 @@ def swap_server():
         next_server = ip_5_server
 
 
-
 def arp_handler(event):
+
+    log.info(f"PRINTIN CONNECTIONS")
+    for con in core.openflow.connections:
+        log.info(f"CONN = {con}")
     packet = event.parsed
     if packet.type == packet.ARP_TYPE:
-        arp = packet.find('arp')
+        arp = packet.find("arp")
         if arp is not None and arp.opcode == arp.REQUEST:
-            log.info(f"ARP request: Who has {arp.protodst}? Tell {arp.protosrc}")
-            resolve_ip(arp.protodst)
+            log.info(
+                f"ARP request: Who has {arp.protodst}? Tell {arp.protosrc}, src = {arp.src}, dest = {arp.dest}"
+            )
 
-            if ip == virtual_ip:
-                assert False ,"TODO"
-            else:
-                arp_reply = of.ofp_packet_out()
+            # if arp.src == virtual_ip:
+            #     assert False, "TODO"
+            # else:
+            # arp_reply = of.ofp_packet_out()
 
             # Fill in your ARP resolution logic here
             # Example:
